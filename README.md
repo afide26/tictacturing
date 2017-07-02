626ee98b0a0deff2e2f2f73ca16e80e78733f0ac
Alan Fidelino <afidedev26@gmail.com>
2017-07-02 17:41:45 +1000

    Initialize repository

---
+++ README.md
@@ -0,0 +1,6 @@
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+Other software used to complet this application are:
+1. Material-UI
+2. Relay
+3. GraphCool
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <div>
+    <h2>Hello React!</h2>
+  </div>, 
+document.getElementById('root'));
+registerServiceWorker();
